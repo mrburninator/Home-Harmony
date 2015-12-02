f02fddced391449fb69d2fd2c7cc7bd0c115f980
salamanderrex <zhouqingyu.rex@gmail.com>
2015-12-02 01:20:30 -0800

    :sleepy: update issue to the new db schema and update add module in issue

--- www/controllers/issue.js
+++ www/controllers/issue.js
@@ -2,55 +2,60 @@ define('controllers/issue.js', [], function () {
   return function controller(cp) {
     cp.register('issueController', function($scope,$firebaseArray) {
       $scope.message = 'Issue !';
+      console.log("scope message is " + $scope.message);
       var baseRef = new Firebase( firebaseURL ); 
       var ref = baseRef.child("/houses/houses1/issues" );
-//      var ref = new Firebase( firebaseURL + "/houses/houses1/issues" );
+      //ar ref = new Firebase( firebaseURL + "/houses/houses1/issues" );
       var authData = ref.getAuth();
 
       $scope.issues = $firebaseArray(ref);
+      
+ 
+      var roomMateRef = baseRef.child("/houses/houses1/users");
+      $scope.roommatelist = $firebaseArray(roomMateRef);
+      $scope.Repeat = false;
 
-      var currentUser = {};
+      var currentUserUid = {};
       if (authData) {
         console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-        currentUser = authData.uid;
+        currentUserUid = authData.uid;
       } else {
         console.log("user is logged out");
       }
       // user is logged out
-      console.log(currentUser);
-       var userName = "dsfdsf";
-       
+      console.log(currentUserUid);
+      
+      
+      var AssignedByName = "temp";
+      var AssignedToName = "temp";
+      $scope.choosenroommate = {"$value":" ","$id":"choose one","$priority":null};
+      var userRef = baseRef.child("/users/"+currentUserUid+"/name");
+      
+      
+      
       //add Issue
       $scope.addIssue = function() {
-        
-        var userRef = baseRef.child("/users/"+currentUser+"/name");
         userRef.once("value", function(data) {
-          userName = data.val();
-          console.log("userName" + userName);
+        AssignedByName = data.val();
+        console.log("AssignedByName" + AssignedByName);
+        console.log("choose roommate " + $scope.choosenroommate.$id + "value is " + $scope.choosenroommate.$value  );
         
-          var userRef2 = baseRef.child("/users/"+$scope.AssignedToUID+"/name");
-          userRef2.once("value", function(data) {
-              userName2 = data.val();
-              console.log("userName2" + userName2);
-            
-               
-              $scope.issues.$add({
+        var userRef2 = baseRef.child("/users/"+$scope.choosenroommate['$id']+"/name");  
+          
+        userRef2.once("value", function(data) {
+            AssignedToName = data.val();
+            console.log("AssignedToName" + AssignedToName);      
+            $scope.issues.$add({
                 Name: $scope.newIssueText ,
                 Done: "false" ,
-                AssignedBy: {userName : userName, userID: currentUser },
-                AssignedTo : {userName: userName2, userID: $scope.AssignedToUID },
+                AssignedBy: {userName : AssignedByName, userID: currentUserUid },
+                AssignedTo : {userName: AssignedToName, userID: $scope.choosenroommate['$id'] },
                 Repeat: $scope.Repeat
+            });       
+          });
           
-            }); 
-           });
         });
-     
-        
-        
-        
-        
-        
-
+    
       };
     });
   }
