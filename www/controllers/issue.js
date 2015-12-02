5e867d40547cb10ac2cd55cf4d3f956f86ae5067
salamanderrex <zhouqingyu.rex@gmail.com>
2015-12-01 20:36:34 -0800

    update adding and displaying in issue, change the issue under the house

--- www/controllers/issue.js
+++ www/controllers/issue.js
@@ -2,7 +2,9 @@ define('controllers/issue.js', [], function () {
   return function controller(cp) {
     cp.register('issueController', function($scope,$firebaseArray) {
       $scope.message = 'Issue !';
-      var ref = new Firebase( firebaseURL + "/issues" );
+      var baseRef = new Firebase( firebaseURL ); 
+      var ref = baseRef.child("/houses/houses1/issues" );
+//      var ref = new Firebase( firebaseURL + "/houses/houses1/issues" );
       var authData = ref.getAuth();
 
       $scope.issues = $firebaseArray(ref);
@@ -16,11 +18,39 @@ define('controllers/issue.js', [], function () {
       }
       // user is logged out
       console.log(currentUser);
+       var userName = "dsfdsf";
+       
+      //add Issue
       $scope.addIssue = function() {
-        $scope.issues.$add({
-          text: $scope.newIssueText ,
-          creater: currentUser
+        
+        var userRef = baseRef.child("/users/"+currentUser+"/name");
+        userRef.once("value", function(data) {
+          userName = data.val();
+          console.log("userName" + userName);
+        
+          var userRef2 = baseRef.child("/users/"+$scope.AssignedToUID+"/name");
+          userRef2.once("value", function(data) {
+              userName2 = data.val();
+              console.log("userName2" + userName2);
+            
+               
+              $scope.issues.$add({
+                Name: $scope.newIssueText ,
+                Done: "false" ,
+                AssignedBy: {userName : userName, userID: currentUser },
+                AssignedTo : {userName: userName2, userID: $scope.AssignedToUID },
+                Repeat: $scope.Repeat
+          
+            }); 
+           });
         });
+     
+        
+        
+        
+        
+        
+
       };
     });
   }
