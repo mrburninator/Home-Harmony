4d51bc141de7fb37a64a1573757835fdcca3f3c8
raghavabboy <raghavabboy@gmail.com>
2015-12-03 18:33:53 -0800

    [Save] Working on shoppingList.

--- www/controllers/issue.js
+++ www/controllers/issue.js
@@ -1,24 +1,20 @@
 define('controllers/issue.js', [], function () {
   return function controller(cp) {
-    cp.register('issueController', ['$scope', '$rootScope', '$firebaseArray',function($scope,$rootScope,$firebaseArray) {
-
+    cp.register('issueController', ['$scope', '$rootScope', '$firebaseArray', function($scope,$rootScope,$firebaseArray) {
 
       $scope.message = 'Issue !';
       console.log("scope message is " + $scope.message);
       var baseRef = new Firebase( firebaseURL );
-      console.log('RootScope Status:', $rootScope.user);
-      var ref = baseRef.child('/houses/' + $rootScope.user.house + '/issues' );
+      var ref = baseRef.child("/houses/houses1/issues" );
       //ar ref = new Firebase( firebaseURL + "/houses/houses1/issues" );
       var authData = ref.getAuth();
 
       $scope.issues = $firebaseArray(ref);
 
-      $scope.choosenroommate = {"$value":" choose some one"};
-      var roomMateRef = baseRef.child('/houses/' + $rootScope.user.house +'/users');
+
+      var roomMateRef = baseRef.child("/houses/houses1/users");
       $scope.roommatelist = $firebaseArray(roomMateRef);
       $scope.Repeat = false;
-      
-      
 
       var currentUserUid = {};
       if (authData) {
@@ -29,64 +25,39 @@ define('controllers/issue.js', [], function () {
       }
       // user is logged out
       console.log(currentUserUid);
-      
+
+
+      var AssignedByName = "temp";
+      var AssignedToName = "temp";
+      $scope.choosenroommate = {"$value":" ","$id":"choose one","$priority":null};
+      var userRef = baseRef.child("/users/"+currentUserUid+"/name");
+
+
 
       //add Issue
-      $scope.addIssue = function() { 
-            console.log("AssignedToName" , $scope.choosenroommate);
+      $scope.addIssue = function() {
+        userRef.once("value", function(data) {
+        AssignedByName = data.val();
+        console.log("AssignedByName" + AssignedByName);
+        console.log("choose roommate " + $scope.choosenroommate.$id + "value is " + $scope.choosenroommate.$value  );
+
+        var userRef2 = baseRef.child("/users/"+$scope.choosenroommate['$id']+"/name");
+
+        userRef2.once("value", function(data) {
+            AssignedToName = data.val();
+            console.log("AssignedToName" + AssignedToName);
             $scope.issues.$add({
                 Name: $scope.newIssueText ,
-                Done: false ,
-                AssignedBy: $rootScope.user.username,
-                AssignedTo : $scope.choosenroommate ,
+                Done: "false" ,
+                AssignedBy: {userName : AssignedByName, userID: currentUserUid },
+                AssignedTo : {userName: AssignedToName, userID: $scope.choosenroommate['$id'] },
                 Repeat: $scope.Repeat
             });
+          });
+
+        });
 
       };
-      
-      
-      //issue filter
-      $scope.issueFilter = function(issue,toggleFlag) {
-        if (toggleFlag)
-          return true;
-        if (issue.AssignedTo.name==$rootScope.user.username)
-        {
-            //console.log(issue);
-            return true;
-        }
-         else return false;
-      };
-      
-      
-      $scope.DisplayMode =  "Only Show my issues"; 
-      
-      $scope.filterToggle = function() {
-            $scope.toggleFlag = $scope.toggleFlag === false ? true: false;
-            if ($scope.toggleFlag) 
-            {
-               $scope.DisplayMode =  "Show all";   
-            } else {
-               $scope.DisplayMode =  "Only Show my issues";
-            }
-        };
-        
-        
-        $scope.showDone = function(data) {
-          //console.log("in showDOne: username is ", $rootScope.user.username, "assign to",data.AssignedTo.name, "data", data);   
-          if ($rootScope.user.username == data.AssignedTo.name && data.Done==false) {
-              return true;
-          }
-              return false;
-        };
-        
-        
-        $scope.markDone = function (issue) {
-          $scope.issues.$remove(issue);
-          issue.Done = true;
-          $scope.issues.$add(issue);
-        };
-        
-        
     }]);
   }
 });
