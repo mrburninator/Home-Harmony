5a43afe1bb3aa46609ca65ab23b04694557b6dc4
salamanderrex <zhouqingyu.rex@gmail.com>
2015-12-06 16:49:02 -0800

    add libaray

--- www/tests/code/spec/homeHarmonySpec.js
+++ www/tests/code/spec/homeHarmonySpec.js
@@ -3,6 +3,43 @@ describe('Home Harmony - Sample Test', function() {
     expect(true).to.equal(true);
   });
 
+
+  
+  
+describe('userAPI', function () {
+    // define variables for the services we want to access in tests
+    var userAPI,
+        $log;
+
+    beforeEach(function () {
+        // load the module we want to test
+        module('app');
+
+        // inject the services we want to test
+        inject(function (_userAPI_, _$log_) {
+            userAPI = _userAPI_;
+            $log = _$log_;
+        })
+    });
+
+    describe('#DoSomething', function () {
+        it('should log the message "something done!"', function () {
+            // Arrange
+            sinon.spy($log, 'info');
+
+            // Act
+            SimpleService.DoSomething();
+    
+            // Assert
+            assert($log.info.calledOnce);
+            assert($log.info.calledWith('something done!'));
+
+            // Cleanup
+            $log.info.restore();
+        });
+    });
+});
+
 });
 
 /*
